f2b9bb2204c01cbb0691e68848b0bd3887631774
Robin Bird <googleoblivion@users.noreply.github.com>
2024-10-01 17:30:05 -0500

    new ping

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -8,11 +8,14 @@
 /**
  * @type {import('../../typings').LegacyCommand}
  */
-module.exports = {
-	name: "ping",
-	// Refer to typings.d.ts for available properties.
 
-	execute(message, args) {
-		message.channel.send({ content: "Pong." });
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
 	},
 };
